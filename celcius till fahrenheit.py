35b4f8c9e65ea7a365c6eaf52182006c211a6c6c
niklasengvall <niklas_engvall@hotmail.com>
2018-11-19 22:46:46 +0100

    Kodexempel, lärobok sid. 50

---
+++ celcius till fahrenheit.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python3.7                          
+# Filnamn: celcius till fahrenheit.py
+# Kodexempel, lärobok sid. 50
+
+# Programmet konverterar från Celcius till Fahrenheit
+
+# Inmatning av gradtal i Celciusskalan
+celcius = float(input('Ange grader i Celcius: '))
+
+# Omvandling till Fahrenheit
+fahrenheit = celcius * 9 / 5 + 32
+
+# Utskrift av resultatet med en decimal
+print('Det blir {:.1f} grader fahrenheit'.format(fahrenheit))
