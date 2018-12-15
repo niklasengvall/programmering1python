6db3ad555db8706020ad073bb06554c1eb7323f1
niklasengvall <niklas_engvall@hotmail.com>
2018-12-15 12:35:22 +0100

    9:e bearbetningen

---
+++ Arbetsbok/övn 5.2 - räkna ner - kap 5, sid. 9.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# Filnamn: övn 5.2 - räkna ner - kap 5, sid 9.py
+
+# Loopar - upprepning, iteration
+# Programmeringsövningar till kapitel 5 - Arbetsbok
+
+# Programmet räknar ner från 100 till 1
+for i in range(100, 0, -1): # slutvärde noll inkluderas inte
+    print(i, end = ' ')
+print('')
+input('\nTryck på en tangent...\n')
---
+++ Arbetsbok/övn 5.3 - udda tal mindre än 20 - kap 5, sid 9.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# Filnamn: övn 5.3 - udda tal mindre än 20 - kap 5, sid 9.py
+
+# Loopar - upprepning, iteration
+# Programmeringsövningar till kapitel 5 - Arbetsbok
+
+# Programmet skriver ut alla udda tal mindre än 20
+for i in range(1, 20, 2): # Vi börjar med 1 i nästa itteration blir det 3
+    print(i, end = ' ')
+print('')
+input('\nTryck på en tangent...\n')
