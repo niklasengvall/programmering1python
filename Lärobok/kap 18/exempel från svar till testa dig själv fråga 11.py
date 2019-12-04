63b89c5711633b388c65813f6432425cb002355c
Niklas Engvall <niklas_engvall@hotmail.com>
2019-12-05 00:34:10 +0100

    Uppdaterade filnamn, fixade övningar och svar

---
+++ Lärobok/kap 18/exempel från svar till testa dig själv fråga 11.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3.8
+
+# Filnamn: exempel från svar till testa dig själv fråga 11.py
+
+# Kapitel 18 - Filhantering 
+# Programmering 1 med Python - Lärobok
+
+# Skriver ut innehållet från en textfils första rad
+fil = open('test2.txt', 'r', encoding = 'utf-8')
+rad = fil.readline()
+fil.close()
+print(rad, end = '') # Skriver ut raden på konsolen
+
+# Skriver ut innehållet från en textfil rad för rad
+fil = open('test2.txt', 'r', encoding = 'utf-8')
+rader = fil.readlines()
+fil.close()
+for rad in rader:
+    print(rad.strip()) # Skriver ut en rad i taget på konsolen
+
+# Skriv ut allt textinnehåll på engång 
+fil = open('test2.txt', 'r', encoding = 'utf-8')
+texten = fil.read()
+fil.close()
+print(texten) # Skriver ut alla rader på konsolen
--- Lärobok/kap 18/kap. 18, sid. 208 - säkrare filhantering.py
+++ Lärobok/kap 18/kap. 18, sid. 208 - säkrare filhantering.py
@@ -12,26 +12,31 @@
 # skriver dit något
 
 # öppnar filen med en säkrare sk kontex öppning, om inte filen 
-# finns skapar den, w = skriver och ersätter tidigare innehåll
+# finns skapas den, w = skriver och ersätter tidigare innehåll
 try:
-    with open('./test.txt', 'w') as f: 
+    # ./ skapar filen test.txt en nivå högre upp i filhierarkin
+    with open('./test.txt', 'w', encoding = 'utf-8') as f:
         rad = input('Skriv en rad (tom rad avslutar): ')
         while rad:
             f.write(rad + '\n')
             rad = input('Skriv en rad (tom rad avslutar): ')   
         f.close()
+# Filen är antingen skrivskyddad eller öppen i ett annat program och systemet kan ej hantera detta utan exklusiv åtkomst
 except PermissionError:
    print('Fel - Kunde inte öppna fil. Antingen är den skrivskyddad eller öppnad med ett annat program.')
 
 # Sedan vi stängt filen öppnar vi den på nytt för läsning och visar innehållet
 #med en while-loop
 try: 
-    # existerar inte filen får vi ett felmeddelande
-    with open('./test.txt', 'r') as f:
+    # Öppna och läs in hela filen    
+    with open('./test.txt', 'r', encoding = 'utf-8') as f:
         filinnehåll = f.read()
+        # Placera filpekaren med 0 steg från filens slut 2
         f.seek(0,2)
+        # Positionen avslöjas med funktionen tell() och står vi i slutet så får vi filstorleken
         storlek = f.tell()
 
+# existerar inte filen får vi ett felmeddelande
 except FileNotFoundError:
     print('Fel - hittar inte filen som ska öppnas.')
 else:
