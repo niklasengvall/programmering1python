36b8e53021bd1ce4646f2aa02df356004ffa6769
Niklas Engvall <niklas_engvall@hotmail.com>
2020-05-07 01:16:18 +0200

    Äntligen fixat klart övning 18.1 och Övning 18.2a

--- Arbetsbok/kap 18/övn 18.2a, sid. 42 - skriv ut textfil.py
+++ Arbetsbok/kap 18/övn 18.2a, sid. 42 - skriv ut textfil.py
@@ -13,73 +13,54 @@
 # Import av modul
 
 # Funktionsdefinitioner
-def meny():
-    print('Vad vill du göra?')
-    print('===================================')
-    print('1. lägga till eller ändra en adress')
-    print('2. söka en adress')
-    print('3. ta bort en adress ur listan')
-    print('4. visa hela registret')
-    print('0. avsluta\n')
+def rubrik():
+    print('Program som skriver ut en textfil till skärmen')
+    print('==============================================')
+    print()
   
 def avsluta():
     exit()
 
-def laggTill():
-    print()
-    nyckel = input('Ange namn: ')
-    värde = input('Ange e-postadress: ')
-    nyckel = nyckel.capitalize()
-    värde = värde.lower()
-    adresser[nyckel] = värde
-    print()
+def fragaFilnamn():
+    # Testa om användaren anger mint ett tecken
+    filnamn = input('Ange filnamn: ')
+    while len(filnamn) == 0:
+        print('FEL: Ett filnamn består av minst ett tecken.')      
+        filnamn = input('Ange filnamn: ')
+    return filnamn
 
-def sok():
-    print()
-    nyckel = input('Ange namn på den adress du vill söka efter: ')
-    nyckel = nyckel.capitalize()
-    if nyckel in adresser:
-        print('Namnet', nyckel, 'finns.')
+def oppnaFil(fn):
+    fn = './' + fn # För att hitta filen i linux 
+    try: 
+        # Öppna och läs in hela filen    
+        with open(fn, 'r', encoding = 'utf-8') as f:
+            filinnehåll = f.read()
+    except FileNotFoundError:
+        print('Fel - hittar inte filen', fn, 'som ska öppnas.')
+        return -1 # Tala om för anropande rutin att det inte gick att öppna filen
     else:
-        print('Ingen med namn', nyckel,'finns.')
-    print()
-
-def tabort():
-    print()
-    nyckel = input('Ange namn på den adress du vill ta bort: ')
-    nyckel = nyckel.capitalize()
-    print('Tar bort addressen', adresser[nyckel])
-    del(adresser[nyckel])
-    print()
-
-def visa():
-    print()
-    for nyckel, värde in adresser.items():
-        print('Namn:', nyckel,'e-post:', värde)
-    print()
+        print(filinnehåll)
+        f.close()
 
     # Huvudprogram
 def main():
-    # Skriv ut meny
-    meny()
-    # Fråga vad användaren vill
-    while True:
-        val = input('Välj 1 - 4 eller 0: ')
-        if val == '0':
-            avsluta()
-        elif val == '1':
-            laggTill()
-            meny()
-        elif val == '2':
-            sok()
-            meny()
-        elif val == '3':
-            tabort()
-            meny()
-        elif val == '4':
-            visa()
-            meny()
-        
+    # Skriv ut rubrik
+    rubrik()
+
+    # Fråga efter filnamn   
+    filnamn = fragaFilnamn()
+    
+    # Öppna filen
+    while resultat := oppnaFil(filnamn) == -1: # Kör loop så länge det inte går att öppna fil 
+        while True: # Fråga om och om igen, tills användaren anger J eller N
+            print('Vill du försöka igen (J) eller avbryta programmet (N)?')
+            svar = input('Svara (J/N)? ')
+            if svar == 'N' or svar == 'n':
+            # Avbryt while-loopen och hoppa ur programmet
+                exit() 
+            elif svar == 'J' or svar == 'j':
+                filnamn = fragaFilnamn()
+                break # För att lämna evighetsloopen och gå tillbaka till loopen ovan
+            
 # Huvudprogram anropas 
-adresser = {}
 main()
